82f5b0f7b7aa78d992a4b31096f3cf9fab643f17
Benjamin Meyers <meyersbs@users.noreply.github.com>
2015-08-11 16:22:48 -0400

    Update mountstats.py

--- mountstats.py
+++ mountstats.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 ##########
 # Author: Benjamin S. Meyers
@@ -20,6 +20,7 @@ curr_read = 0
 curr_write = 0
 avgs = {}
 all_ips = []
+out_files = {}
 
 
 # Get the IP Address from a given string. If the IP Address is new,
@@ -27,30 +28,34 @@ all_ips = []
 def get_curr_ip(ip_block):
 	global curr_ip, all_ips
 	curr_ip = ip_block.split(':')[0]
-	if curr_ip not in all_ips:
-		all_ips.append(curr_ip)
-		avgs[curr_ip] = [[], [], [], []] # vals, avg, vals, avg
+	if curr_ip not in all_ips: # If this IP hasn't been seen yet
+		all_ips.append(curr_ip) # Make note that we've seen it
+		avgs[curr_ip] = [[], [], [], []] #read_vals, read_avg, write_vals, write_avg
+		new_file = open(str(curr_ip) + '.out', 'a') # Create an output file named IP.out
+		out_files[new_file.name] = new_file
 	return curr_ip
 
 
 def main():
-	global curr_data, all_ips, curr_read, curr_write
+	global curr_data, all_ips, curr_read, curr_write, out_files
 	if len(sys.argv) == 1:
 		while True:
-			out_file = open('mountstats.out', 'a')
+			out_file = open('all_ips.out', 'a')
 			for line in fileinput.input():
 				split_line = line.split()
-				if split_line != ['####', 'Sleeping', '####']:
+				if split_line != ['####', 'Sleeping', '####']: # Until we see ['####', 'Sleeping', '####']
 					for item in split_line:
-						curr_data.append(item)
+						curr_data.append(item) # Temporarily store the line
 				else:
+					# Get timestamp.
 					ts = time.time()
 					timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 					count = 0
+					# For every 45 items in the list of input
 					while count < 45 and len(curr_data) >= 45:
-						curr_read = curr_data[25]
-						curr_write = curr_data[43]
-						ip = get_curr_ip(curr_data[0])
+						curr_read = curr_data[25] # Get the current read value
+						curr_write = curr_data[43] # Get the current write value
+						ip = get_curr_ip(curr_data[0]) # Get the current IP Address
 
 						# Calculate read average.
 						temp_read_total = 0
@@ -70,8 +75,13 @@ def main():
 							temp_write_total += float(item)
 						avgs[ip][3] = float(temp_write_total) / temp_write_count
 
-						# Print output.
+						# Print output to single file
 						out_file.write(str(timestamp) + '|' + str(ip) + '|' + str(avgs[ip][1]) + '|' + str(avgs[ip][3]) + '\n')
+						for item in out_files.keys(): # Print output to multiple files for each IP
+							if item == str(ip) + '.out':
+								out_files[item].write(str(timestamp) + '|' + str(ip) + '|' + str(avgs[ip][1]) + '|' + str(avgs[ip][3]) + '\n') # Write CSV: Timestamp|IP|Read|Write
+								break	
+						# Print to stdout.					
 						print str(timestamp) + '|' + str(ip) + '|' + str(avgs[ip][1]) + '|' + str(avgs[ip][3])
 
 						# Prepare data for next iteration.
